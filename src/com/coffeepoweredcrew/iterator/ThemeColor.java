e95474e9aeeaabfd2ed81655e373d64dad41c1e0
nischal <nischal.bhatt.87@gmail.com>
2022-04-10 01:46:40 +0800

    committing iterator design pattern

---
+++ src/com/coffeepoweredcrew/iterator/ThemeColor.java
@@ -0,0 +1,35 @@
+package com.coffeepoweredcrew.iterator;
+
+//This enum represents the aggregate from iterator pattern
+public enum ThemeColor {
+
+	RED,
+	ORANGE,
+	BLACK,
+	GREEN,
+	MAHOGANY,
+	WHITE;
+
+	public static Iterator<ThemeColor> getIterator()
+	{
+		return new ThemeColorIterator();
+	}
+	private static class ThemeColorIterator implements Iterator<ThemeColor> {
+
+		private int position;
+		
+		@Override
+		public boolean hasNext() {
+			return position < ThemeColor.values().length;
+		}
+
+		@Override
+		public ThemeColor next() {
+			
+		    return ThemeColor.values()[position++];
+		    
+		}
+		
+	}
+}
+
